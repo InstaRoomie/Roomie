a1a734a6de158119066181a7f02a39a5eed60e74
Dankller <danielhungkim@gmail.com>
2016-02-18 12:01:21 -0800

    [feature]Adds login and signup/profile creation views

---
+++ public/auth/auth.js
@@ -0,0 +1,30 @@
+angular.module('roomie.auth', [])
+
+.controller('AuthController', function ($scope, $window, $location, Auth){
+  $scope.user = {};
+
+  $scope.signup = function(){
+    Auth.signup($scope.user)
+    .then(function (token){
+      $window.localStorage.setItem('com.roomie', token);
+      $location.path('/main');
+    })
+    .catch(function (error){
+      console.error(error);
+    });
+  };
+
+
+  $scope.signin = function(){
+    Auth.signin($scope.user)
+    .then(function (token){
+      $window.localStorage.setItem('come.roomie', token);
+      $location.path('/main');
+    })
+    .catch(function (error){
+      console.error(error);
+    });
+  };
+
+
+});
