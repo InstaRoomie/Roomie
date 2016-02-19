c07e46cf05f91785138fac504eb4233353d4a5b1
Danny Rizko <drizko07@gmail.com>
2016-02-18 18:52:48 -0800

    Adds main route + prelim getUser

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,1 +1,10 @@
-// query db to get user info that displays profile info
+var User = require('../models/user.js'),
+    Users = require('../collections/users.js'),
+    Promise    = require('bluebird'),
+    jwt  = require('jwt-simple');
+
+module.exports = {
+  getUser: funciton(req, res, next){
+    console.log(req.token);
+  }
+}
