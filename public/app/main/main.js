73a494af86b8b457fcfc74203644a60219c1dee3
Ethan Rubio <ethanrubio@gmail.com>
2016-02-22 10:32:54 -0800

    adds approve and decline functionality with proper data reception

--- public/app/main/main.js
+++ public/app/main/main.js
@@ -3,21 +3,37 @@ angular.module('roomie.main', [])
 .controller('MainController', function($scope, State, $window) {
   $scope.data = { firstname: 'kyle', lastname: 'kyle', age: 23, gender: 'M', url: 'https://journalism.missouri.edu/wp-content/uploads/2011/10/greeley-kyle-200x300.jpg', aboutme: 'I like food'};
 
+  $scope.dataArray = [];
+
   console.log('inside main controller scope token ' + $scope.token);
-  State.getData().then(function(data) {
-    $scope.data = data;
-  });
+
+  $scope.getData = function() {
+    State.getData().then(function(data) {
+      $scope.dataArray = data;
+      $scope.data = $scope.dataArray[0];
+    });
+  }
 
   $scope.approve = function() {
-    State.approve($scope.data).then(function(data) {
-      $scope.data = data;
+    State.approve($scope.data).then(function() {
+      $scope.dataArray.shift();
+      if ($scope.dataArray.length < 1) {
+        $scope.getData();
+      }
+      $scope.data = $scope.dataArray[0];
     });
   };
 
   $scope.decline = function() {
     State.decline($scope.data).then(function(data) {
-      $scope.data = data;
+      $scope.dataArray.shift();
+      if ($scope.dataArray.length < 1) {
+        $scope.getData();
+      }
+      $scope.data = $scope.dataArray[0];
     });
   };
+
+  $scope.getData();
 
 });
