6814dfadf8c55631b965166164d69150b70bc394
Ethan Rubio <ethanrubio@gmail.com>
2016-02-23 15:29:59 -0800

    adds check for null and changes client if there are no more roomies

--- public/app/main/main.js
+++ public/app/main/main.js
@@ -1,16 +1,30 @@
 angular.module('roomie.main', [])
 
 .controller('MainController', function($scope, $mdMedia, State, $window) {
-  $scope.data = { firstname: 'kyle', lastname: 'kyle', age: 23, gender: 'M', url: 'https://journalism.missouri.edu/wp-content/uploads/2011/10/greeley-kyle-200x300.jpg', aboutme: 'I like food'};
+  /*$scope.data = { firstname: 'kyle', lastname: 'kyle', age: 23, gender: 'M', url: 'https://journalism.missouri.edu/wp-content/uploads/2011/10/greeley-kyle-200x300.jpg', aboutme: 'I like food'};*/
+
+  $scope.data;
 
   $scope.dataArray = [];
 
   console.log('inside main controller scope token ' + $scope.token);
 
+  $scope.seenAllTruth = function() {
+    var result = false;
+    if (State.seenAllTruth() === true) {
+      result = true;
+    }
+    return result;
+  };
+
   $scope.getData = function() {
     State.getData().then(function(data) {
-      $scope.dataArray = data;
-      $scope.data = $scope.dataArray[0];
+      if (data.length < 1) {
+        console.log('You\'ve seen all the roomies!');
+      } else {
+        $scope.dataArray = data;
+        $scope.data = $scope.dataArray[0];
+      }
     });
   }
 
