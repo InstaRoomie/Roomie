1f74c2bb853dd2d772c2e444cb869ede1ecfc5be
Ethan Rubio <ethanrubio@gmail.com>
2016-02-22 16:39:33 -0800

    adds getting friends/contacts functionality

--- server/controllers/friendController.js
+++ server/controllers/friendController.js
@@ -1,33 +1,40 @@
 var Promise = require('bluebird');
-var helpers = require('../util/helpers.js');
+var util = require('../util/helpers.js');
 var Friend = require('../models/friend.js');
 var Friends = require('../collections/friends.js');
+var _ = require('underscore');
+var helpers = require('./profileController.js');
 
 module.exports = {
 
   getFriend: function(req, res) {
 
-    helpers.decode(req, res, function(userThatSwiped) {
+    util.decode(req, res, function(userThatSwiped) {
       console.log('From getFriend() using decoded: ', userThatSwiped.id);
 
-      new Friend({ user_id: userThatSwiped.id })
-        .fetch()
-          .then(function(friend) {
-              if (!friend) {
-                console.log('No friends found!')
-                res.status(200).send('No friends found');
-              } else {
-                console.log('Friend(s) found  ', friend)
-                res.status(200).send(friend);
-              }
+      var contactList = [];
+      var usersToSend = [];
+
+      helpers.checkYes(req, function(item) {
+          item.forEach(function(friend) {
+            if (friend.user_id === userThatSwiped.id) {
+              contactList.push(friend.friend)
+            } else {
+              contactList.push(friend.user_id);
+            }
+          });
+          helpers.getDifferences(contactList, function(item) {
+            item.forEach(function(some) {
+              usersToSend.push(some);
             })
-              .catch(function(err) {
-                console.log('Finding friends failed... ', err);
-                res.status(500).send('Finding friends failed...');
-              });
+              res.status(200).send(usersToSend);
+            });
+        })
+          .catch(function(err) {
+              console.log('This is an error from finding friends ', err);
+            });
+      });
 
-    });
-
-  }
+    }
 
 };
